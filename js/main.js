9dac1fe0fb0b284d94f679a7294a1aa837c20ef8
Rajiv Bose <github@dev.lesbose.com>
2014-04-05 19:57:29 +0100

    add boilerplate

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.TrueColours = {
+
+    Models: {},
+
+    init: function () {
+        'use strict';
+
+        console.log('Hello from runner!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+
+    TrueColours.init();
+});
