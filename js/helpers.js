761ed2e906898ec4876ed1258791045d41e49601
Rajiv Bose <github@dev.lesbose.com>
2014-04-06 01:04:54 +0100

    add sin, cos and atan for mean calculation

--- js/helpers.js
+++ js/helpers.js
@@ -3,6 +3,54 @@ TrueColours = TrueColours || {};
 
 TrueColours.Helpers = TrueColours.Helpers || {
 
+    getArctanMean: function (sinMean, cosMean) {
+        'use strict';
+
+        var arctanMean = Math.atan2(sinMean, cosMean);
+
+        // if sin is +ve and cos is -ve (ie 2nd quadrant)
+        if (sinMean > 0 && cosMean < 0) {
+            return 180 - arctanMean;
+        }
+
+        // if sin is -ve and cos is -ve (ie 3rd quadrant)
+        if (sinMean < 0 && cosMean < 0){
+            return 180 + arctanMean;
+        }
+
+        // if sin is -ve and cos is +ve (ie 4th quadrant)
+        if (sinMean < 0 && cosMean > 0){
+            return 360 - arctanMean;
+        }
+
+        return arctanMean;
+    },
+
+
+    getCosMean: function (bedTimes) {
+        'use strict';
+
+        var X = 0;
+
+        _.each(bedTimes, function (bedTime) {
+            X += bedTime.getCos();
+        }, this);
+
+        return X / bedTimes.length;
+    },
+
+    getSinMean: function (bedTimes) {
+        'use strict';
+
+        var Y = 0;
+
+        _.each(bedTimes, function (bedTime) {
+            Y += bedTime.getSin();
+        }, this);
+
+        return Y / bedTimes.length;
+    },
+
     getCircularMean: function (listOfTimes) {
         'use strict';
 
@@ -12,7 +60,14 @@ TrueColours.Helpers = TrueColours.Helpers || {
             bedTimes.push(new TrueColours.BedTime(time));
         }, this);
 
-        return bedTimes;
+
+        var cosMean = this.getCosMean(bedTimes); // X
+        var sinMean = this.getSinMean(bedTimes); // Y
+
+        var angleMean = this.getArctanMean(sinMean, cosMean);
+
+
+        return angleMean;
     }
 
 };
